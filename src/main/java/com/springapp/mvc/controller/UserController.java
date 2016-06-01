ae4992b6f3551d7ee4da131a9534e1c5c9cf6779
arifen <shamsuddoharefin.dsi@gmail.com>
2016-06-01 15:00:43 +0600

    include log4j,spring-security,first run dispatcher servlet,then spring-security,password as sha-256,spring user authentication

--- src/main/java/com/springapp/mvc/controller/UserController.java
+++ src/main/java/com/springapp/mvc/controller/UserController.java
@@ -1,5 +1,6 @@
 package com.springapp.mvc.controller;
 
+import com.springapp.mvc.model.Role;
 import com.springapp.mvc.model.User;
 import com.springapp.mvc.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,9 +8,13 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.beans.PropertyEditorSupport;
 
 /**
  * Created by arifen on 5/25/16.
@@ -20,7 +25,17 @@ public class UserController {
     @Autowired
     @Qualifier("userService")
     private UserService userService;
-
+    @InitBinder
+    protected void initBinder(WebDataBinder binder) throws Exception {
+        binder.registerCustomEditor(Role.class, "role", new PropertyEditorSupport() {
+            @Override
+            public void setAsText(String text) {
+                //Role role = childService.findById(Long.parseLong(text));
+                Role role = new Role();
+                setValue(role);
+            }
+        });
+    }
     @RequestMapping(value = { "/usercreate" })
     public String usercreate() {
         return "createuser";
